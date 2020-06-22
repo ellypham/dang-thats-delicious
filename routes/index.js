d60483aacec0399a599d0824f9edccd5978b93d7
ellypham <ellypham@gmail.com>
2020-06-22 10:34:41 -0400

    Add pagination

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const { catchErrors } = require("../handlers/errorHandlers");
 
 router.get("/", catchErrors(storeController.getStores));
 router.get("/stores", storeController.getStores);
+router.get("/stores/page/:page", storeController.getStores);
 router.get("/add", authController.isLoggedIn, storeController.addStore);
 router.post(
   "/add",
